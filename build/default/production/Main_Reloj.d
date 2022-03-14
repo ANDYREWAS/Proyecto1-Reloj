f017fb90cb61a0b73b8838303cbc35ae3dc067d7
ANDYREWAS <99231727+ANDYREWAS@users.noreply.github.com>
2022-03-14 12:01:31 -0600

    Multiplexado

---
+++ build/default/production/Main_Reloj.d
@@ -0,0 +1,1 @@
+build/default/production\Main_Reloj.i: Main_Reloj.s
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Proyecto__1_-_Reloj.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Proyecto__1_-_Reloj.X.production.hex
